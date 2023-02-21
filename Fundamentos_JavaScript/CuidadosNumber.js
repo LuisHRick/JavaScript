0dfc96f9065c930377703a5f3087a5925005a357
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-20 22:40:40 -0300

    more

---
+++ Fundamentos_JavaScript/CuidadosNumber.js
@@ -0,0 +1,6 @@
+
+console.log(7 / 0) // retorna Infinity
+console.log('10' / 2) // caso consiga reconhecer que pode ser um number, ele faz o calculo
+console.log('string' * 2) // retorna NaN -> Not a Number
+console.log(0.1 * 0.7) // não tão preciso, para economizar tempo
+console.log(10.325.toFixed(2))
