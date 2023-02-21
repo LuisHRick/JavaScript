355e01c2f9c08f5b05b426399880072b960885a3
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-20 21:02:32 -0300

    more

---
+++ Fundamentos_JavaScript/TipagemFraca.js
@@ -0,0 +1,15 @@
+let string = 'legal'
+console.log(string, typeof string)
+
+let numero = 2
+console.log(numero, typeof numero)
+
+let numerostring = '2'
+console.log(numerostring, typeof numerostring)
+
+numero = 3.14
+console.log(numero)
+console.log(typeof numero)
+
+let bool = true
+console.log(bool, typeof bool)
