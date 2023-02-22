2b8b32e4f116c7d06d19b0618d01e0f3a4e36d13
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-22 13:11:20 -0300

    more

---
+++ Fundamentos_JavaScript/Hoisting.js
@@ -0,0 +1,5 @@
+console.log('a =', a) // {undefined} declarou a variável, só não deu valor a ela.
+var a = 2
+console.log('a =', a)
+
+// Hoisting => a declaração da variável sempre ocorre no topo do código
