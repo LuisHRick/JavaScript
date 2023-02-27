23cc59712c8b80f90b297f0b664ca5f52290f04e
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-27 17:44:52 -0300

    more

---
+++ Estruturas_de_controle_JavaScript/For.js
@@ -0,0 +1,17 @@
+let contator = 1
+
+while(contator <= 10){
+    contator++
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+// Iterando com For
+
+const notas = [1, 4, 5, 6, 2, 5, 3]
+
+for(i=0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`)
+}
