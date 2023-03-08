894f21e08159964608b595e1eebf9d450e8ee82c
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-08 16:37:36 -0300

    more

---
+++ Funções_JavaScript/IIFE.js
@@ -0,0 +1,9 @@
+// IIFE -> Immediately Invoked Function Expression
+
+// Função que escapa do escopo global
+
+
+(function () {
+    console.log('Será executado na hora da declaração')
+    console.log('Foge do escopo abrangente')
+})()
