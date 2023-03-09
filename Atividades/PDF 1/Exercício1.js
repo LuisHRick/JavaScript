d5037442ccfa6d216d942596e2797096632df59e
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-09 13:30:46 -0300

    more

---
+++ Atividades/PDF 1/Exercício1.js
@@ -0,0 +1,9 @@
+soma = (x, y) => x + y
+subtrai = (x, y) => x - y
+multiplica = (x, y) => x * y
+divide = (x, y = 1) => x / y
+
+console.log(soma(3, 4))
+console.log(subtrai(3, 4))
+console.log(multiplica(3, 4))
+console.log(divide(2, 4))
