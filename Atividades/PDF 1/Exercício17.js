704dfc4b93f59e05e2256fcff592e387590cbe78
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-12 08:34:43 -0300

    more

---
+++ Atividades/PDF 1/Exercício17.js
@@ -0,0 +1,11 @@
+function aumento(salario, valor) {
+    switch (valor) {
+        case 10: return salario = salario + salario * 0.10
+        case 15: return salario = salario + salario * 0.15
+        case 20: return salario = salario + salario * 0.20
+    }
+}
+
+console.log(aumento(1000, 20))
+console.log(aumento(1500, 10))
+console.log(aumento(2000, 15))
