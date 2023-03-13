9c7eca7e527b929dd9f86bd5cddaa8f4116afc24
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-13 13:39:04 -0300

    more

--- Objetos/OO.js
+++ Objetos/OO.js
@@ -15,5 +15,18 @@ Objeto = {
 
 objeto.processamento()  // Foco passou a ser o objeto
 
-// Princípios importantes
-//1. abstração: trazer elementos do mundo real ao código de forma útil e simples
+/* Princípios importantes
+ 1. Abstração: 
+    trazer elementos do mundo real ao código de forma útil e simples.
+    abstração = abstrair          abstração = abstrato
+ 2. Encapsulamento
+    mostrar só o que for necessário para a útilização do objeto
+    adaptar o objeto.
+3. Herança
+    Reciclagem de código para evitar repetição desnecessária.
+4. Polimorfismo
+    Formas variadas para um mesmo objeto.
+    Ex:
+    pai = Carro
+    filhos = ferrari, volkswagem...
+*/
