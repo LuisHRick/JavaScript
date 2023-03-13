08ffad70bc1decce0f15b6c1c25bad1e6019196a
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-13 13:38:02 -0300

    more

---
+++ Objetos/OO.js
@@ -0,0 +1,19 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+Objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()  // Foco passou a ser o objeto
+
+// Princípios importantes
+//1. abstração: trazer elementos do mundo real ao código de forma útil e simples
