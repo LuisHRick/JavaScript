7f1a23c887e1fa890f004258e80d225bce7b9dc1
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-20 13:28:42 -0300

    fix

--- Objetos/PrevenindoModificações.js
+++ Objetos/PrevenindoModificações.js
@@ -15,6 +15,17 @@ delete produto.preco
 console.log(produto)
 
 // Object.seal
+
+// Permite altera, porém, não permite deletar ou adicionar
 const pessoa = { nome: 'Ju', idade: 35 }
 Object.seal(pessoa)
 console.log('selado:', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+// Object.freeze
+
+// Não se pode alterar nada
