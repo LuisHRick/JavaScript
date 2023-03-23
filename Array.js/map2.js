57ac6ca262b14e9dc18c654484706b2096b75cec
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-23 17:04:17 -0300

    more

---
+++ Array.js/map2.js
@@ -0,0 +1,25 @@
+const carrinho = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os peços
+
+precos = carrinho.map(function (e) {
+    valor = JSON.parse(e)
+    return valor['preco']
+})
+
+precos.forEach(function (valor) {
+    console.log(`Preço: R$ ${valor}`)
+})
+
+// Resolução do video
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = produto => produto.preco
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(resultado)
