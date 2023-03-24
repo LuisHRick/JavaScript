5ec58e8537eff93a959c17728ff64c0e28bc8130
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-24 17:43:35 -0300

    more

---
+++ Array.js/FlatMap.js
@@ -0,0 +1,30 @@
+// FlatMap -> Map + Concat
+
+const escola = [{
+    nome: 'Turma m1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: 'Turma m2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1) // 1 array com 2 arrays
+
+console.log(escola.flatMap(getNotasDaTurma)) // 1 array
+
