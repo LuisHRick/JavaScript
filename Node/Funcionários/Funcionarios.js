a097f0a53678afe44c53b2290e3944d121dfaf6c
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-28 16:59:10 -0300

    more

--- Node/Funcionários/Funcionarios.js
+++ Node/Funcionários/Funcionarios.js
@@ -22,3 +22,20 @@ axios.get(url).then(response => {
     })
     console.log(FuncionarioMenorSalario.funcionario)
 })
+
+// Resolução do vídeo
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+    console.log(func)
+})
