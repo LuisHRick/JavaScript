6610cc5e8d35ab7151374b10f06d558cc107b6a4
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-28 13:42:21 -0300

    more

---
+++ Node/MódulosTerceiros.js
@@ -0,0 +1,10 @@
+// Como instalar módulos de terceiros
+// Verificar o pacote Nodemon
+
+// npm - node package manager
+// i - install
+
+// npm i lodash
+
+const lod = require('lodash') // Forma de usar o módulo
+setInterval(() => console.log(lod.random(1, 1000)), 2000)
